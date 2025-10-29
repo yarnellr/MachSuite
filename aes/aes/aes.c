e40f95e98443f327e61856da5f3e8d53b93d1957
Richard Yarnell <richardyarnell@knights.ucf.edu>
2025-10-29 10:47:54 -0400

    fix compile issues

--- aes/aes/aes.c
+++ aes/aes/aes.c
@@ -131,7 +131,7 @@ void aes_shiftRows(uint8_t *buf)
     i = buf[1]; buf[1] = buf[5]; buf[5] = buf[9]; buf[9] = buf[13]; buf[13] = i;
     i = buf[10]; buf[10] = buf[2]; buf[2] = i;
     j = buf[3]; buf[3] = buf[15]; buf[15] = buf[11]; buf[11] = buf[7]; buf[7] = j;
-    j = buf[14]; buf[14] = buf[6]; buf[6]  = j;
+    j = buf[14]; buf[14] = buf[6]; buf[6] = j;
 
 } /* aes_shiftRows */
 
